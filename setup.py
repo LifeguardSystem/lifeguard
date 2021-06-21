daeaa0d148924440e5edd409b71ba26430521d9e
Diego Rubin <contact@diegorubin.dev>
2021-06-21 09:59:28 -0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="lifeguard",
-    version="0.0.20",
+    version="0.0.21",
     url="https://github.com/LifeguardSystem/lifeguard",
     author="Diego Rubin",
     author_email="contact@diegorubin.dev",
