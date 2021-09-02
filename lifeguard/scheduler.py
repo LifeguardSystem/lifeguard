e0071c15b0f23a4de473406bf4bceae22ed327fe
Diego Rubin <contact@diegorubin.dev>
2021-09-02 09:18:22 -0300

    adding more options to schedule param

--- lifeguard/scheduler.py
+++ lifeguard/scheduler.py
@@ -1,16 +1,32 @@
 import time
 import traceback
+
 import schedule
 
-from lifeguard.validations import VALIDATIONS
 from lifeguard.logger import lifeguard_logger as logger
+from lifeguard.validations import VALIDATIONS
 
-valid_time_periods = [
+VALID_TIME_PERIODS = [
     "seconds",
     "minutes",
     "hours",
     "days",
     "weeks",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+]
+
+MOMENTS = [
+    "second",
+    "minute",
+    "hour",
+    "day",
+    "week",
     "monday",
     "tuesday",
     "wednesday",
@@ -26,10 +42,15 @@ def configure_validations():
         content = VALIDATIONS[validation]
         if "every" in content["schedule"]:
             time_period = get_time_period(content)
-            if time_period in valid_time_periods:
+            if time_period in VALID_TIME_PERIODS:
                 job_instance = schedule.every(content["schedule"]["every"][time_period])
                 job_func = get_dynamic_job_func(job_instance, time_period)
                 job_func.do(content["ref"])
+        if "at" in content["schedule"]:
+            time_moment = get_time_moment(content)
+            if time_moment in MOMENTS:
+                moment = getattr(schedule.every(), time_moment)
+                moment.at(content["schedule"]["at"][time_moment]).do(content["ref"])
 
 
 def get_dynamic_job_func(job_instance, time_period):
@@ -40,6 +61,12 @@ def get_time_period(content):
     keys = content["schedule"]["every"].keys()
     time_period = list(keys)[0]
     return time_period
+
+
+def get_time_moment(content):
+    keys = content["schedule"]["at"].keys()
+    time_moment = list(keys)[0]
+    return time_moment
 
 
 def start_scheduler():
--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch, MagicMock
 
-from lifeguard.scheduler import configure_validations
+from lifeguard.scheduler import configure_validations, VALID_TIME_PERIODS, MOMENTS
 
 mock_ref = MagicMock(name="ref")
 
@@ -36,3 +36,62 @@ class TestScheduler(unittest.TestCase):
         configure_validations()
 
         mock_schedule.every.assert_not_called()
+
+    @patch(
+        "lifeguard.scheduler.VALIDATIONS",
+        {"example": {"ref": mock_ref, "schedule": {"at": {"hour": "10:00"}}}},
+    )
+    @patch("lifeguard.scheduler.schedule")
+    def test_configure_validations_at_specific_minute(self, mock_schedule):
+        mock_do = MagicMock(name="do")
+
+        mock_at = MagicMock(name="at")
+        mock_at.at.return_value = mock_do
+
+        mock_return_of_every = MagicMock(name="mock_return_of_every")
+        mock_schedule.every.return_value = mock_return_of_every
+        mock_return_of_every.hour = mock_at
+
+        configure_validations()
+
+        mock_schedule.every.assert_called()
+        mock_at.at.assert_called_with("10:00")
+        mock_do.do.assert_called_with(mock_ref)
+
+    def test_valid_time_periods(self):
+        self.assertEqual(
+            VALID_TIME_PERIODS,
+            [
+                "seconds",
+                "minutes",
+                "hours",
+                "days",
+                "weeks",
+                "monday",
+                "tuesday",
+                "wednesday",
+                "thursday",
+                "friday",
+                "saturday",
+                "sunday",
+            ],
+        )
+
+    def test_moments(self):
+        self.assertEqual(
+            MOMENTS,
+            [
+                "second",
+                "minute",
+                "hour",
+                "day",
+                "week",
+                "monday",
+                "tuesday",
+                "wednesday",
+                "thursday",
+                "friday",
+                "saturday",
+                "sunday",
+            ],
+        )
