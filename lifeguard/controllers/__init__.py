73312c50f6d17a688f283ee162d44a37bda04194
MrLeonardDude <leopagnez@gmail.com>
2022-04-19 23:18:43 -0300

    remove unnecessary build_import

--- lifeguard/controllers/__init__.py
+++ lifeguard/controllers/__init__.py
@@ -219,17 +219,6 @@ def load_custom_controllers():
     """
     Load custom controllers from application path
     """
-
-    def build_import(path, cur_name):
-        """Returns python like import name"""
-        head, tail = os.path.split(path)
-        if tail:
-            cur_name = f"{tail}.{cur_name}"
-        final_name = cur_name
-        if head:
-            final_name = build_import(head, cur_name)
-        return final_name
-
     sys.path.append(LIFEGUARD_DIRECTORY)
 
     if not os.path.exists(os.path.join(LIFEGUARD_DIRECTORY, "controllers")):
