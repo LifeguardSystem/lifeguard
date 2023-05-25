83ba21ec6a91efd3bbf79cbfeddd062f4f2f0840
Diego Rubin <contact@diegorubin.dev>
2023-05-25 12:01:43 -0300

    chore: updating readme

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="lifeguard",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/LifeguardSystem/lifeguard",
     author="Diego Rubin",
     author_email="contact@diegorubin.dev",
@@ -10,7 +14,16 @@ setup(
     scripts=["bin/lifeguard"],
     include_package_data=True,
     description="Application to monitor your systems and give you the security to sleep peacefully at night",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=["flask", "schedule", "gunicorn", "requests", "tabulate"],
-    classifiers=["Development Status :: 3 - Alpha"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Monitoring",
+    ],
     packages=find_packages(),
 )
